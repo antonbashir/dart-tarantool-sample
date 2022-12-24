f0ed73e92199ecd1368fb0820ff2dd29eed2bd1d
anton <anton@local.local>
2022-12-24 22:20:01 +0200

    initial

--- module/user.lua
+++
@@ -1,6 +0,0 @@
-initliazeUser = function(user, password)
-  box.once("initialize-user", function()
-    box.schema.user.create(user, { password = password })
-    box.schema.user.grant(user, 'read,write,execute,create,alter,drop,replication', 'universe', nil)
-  end)
-end
