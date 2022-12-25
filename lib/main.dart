d737a6d374b2e1480f53b72e1c3e7c8fe7e0fc8f
anton.bashirov <anton.sh.local@gmail.com>
2022-12-25 13:36:23 +0200

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:tarantool_storage/tarantool_storage.dart';
 
 Future<void> main(List<String> args) async {
   final storage = Storage()..boot(BootstrapScript(defaultStorageConfiguration()), defaultMessageLoopConfiguration());
   print(await storage.executor().space(280).length());
+  storage.shutdown();
+  exit(0);
 }
